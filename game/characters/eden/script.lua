5a351da52568faa7d4b2b31ace93f2f812e06c9d
GrandOichii <i.hawk@yandex.ru>
2022-09-06 15:38:47 +0300

    Started filling bad items list for Eden

--- game/characters/eden/script.lua
+++ game/characters/eden/script.lua
@@ -1,3 +1,24 @@
+local BAD_EDEN_CARDS = {
+    'Experimental Treatment',
+    'R Key',
+    'Pageant Boy',
+    'Pandora\'s Box',
+    'Glass Canon',
+    'The D4',
+    'Mom\'s Shovel',
+}
+
+function IsBadEdenCard(cardName)
+    for _, name in ipairs(BAD_EDEN_CARDS) do
+        if name == cardName then
+            return true
+        end
+    end
+    return false
+end
+
+local EDEN_CHOICE_AMOUNT = 3
+
 function Eden_use(host)
     local owner = getTopOwner(host)
     addPlayableCount(host, owner.id)
@@ -5,15 +26,21 @@ end
 
 function Eden_game_start(host)
     local owner = getTopOwner(host)
-    local cards = topCardsOf(host, TREASURE_DECK, 3)
     local message = 'Choose:'
     local choices = {}
-    for i, card in ipairs(cards) do
-        message = message..'${'..card.name..'}, '
-        choices[#choices+1] = i..'- ${'..card.name..'}'
+    local cards = {}
+    local count = 0
+    while count ~= EDEN_CHOICE_AMOUNT do
+        local tc = topCardsOf(host, TREASURE_DECK, 1)[1]
+        if not IsBadEdenCard(tc.name) then
+            count = count + 1
+            cards[#cards+1] = tc
+            choices[#choices+1] = count..'- ${'..tc.name..'}'
+            message = message..'${'..tc.name..'}, '
+        end
+        putFromTopToBottom(host, TREASURE_DECK, 1)
     end
     local choice = requestSimpleChoice(host, owner.id, message, choices)
-    putFromTopToBottom(host, TREASURE_DECK, 3)
     local choiceID = -1
     for i, c in ipairs(choices) do
         if choice == c then
